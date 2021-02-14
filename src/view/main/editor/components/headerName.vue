52f808260b41077788307688b18d000210af817a
BackRunner <dev@backrunner.top>
2021-02-14 19:46:51 +0800

    feat: add tooltip

--- src/view/main/editor/components/headerName.vue
+++ src/view/main/editor/components/headerName.vue
@@ -1,11 +1,40 @@
 <template>
   <div class="monaco-header__name">
-    <span :class="{
-      'monaco-header__name_editing': editable,
-      }" ref="name" :contenteditable="editable">{{ name }}</span>
-    <i class="el-icon-edit-outline" @click="handleEditClick" v-show="!editable"></i>
-    <i class="el-icon-close" v-show="editable" @click="handleCancelClick"></i>
-    <i class="el-icon-check" v-show="editable" @click="handleConfirmClick"></i>
+    <span
+      :class="{
+        'monaco-header__name_editing': editable,
+      }"
+      ref="name"
+      :contenteditable="editable"
+      >{{ name }}</span
+    >
+    <el-tooltip
+      class="item"
+      effect="light"
+      :open-delay="1000"
+      :content="$t('editor.name.edit')"
+      placement="bottom"
+    >
+      <i class="el-icon-edit-outline" @click="handleEditClick" v-show="!editable"></i>
+    </el-tooltip>
+    <el-tooltip
+      class="item"
+      effect="light"
+      :open-delay="1000"
+      :content="$t('editor.cancel')"
+      placement="bottom"
+    >
+      <i class="el-icon-close" v-show="editable" @click="handleCancelClick"></i>
+    </el-tooltip>
+    <el-tooltip
+      class="item"
+      effect="light"
+      :open-delay="1000"
+      :content="$t('editor.save')"
+      placement="bottom"
+    >
+      <i class="el-icon-check" v-show="editable" @click="handleConfirmClick"></i>
+    </el-tooltip>
   </div>
 </template>
 
