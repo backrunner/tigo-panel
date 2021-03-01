d049ed8274f1cf22a2d05bd82b638042c02dec6b
BackRunner <dev@backrunner.top>
2021-03-01 15:44:10 +0800

    feat: add policy switch

--- src/view/main/oss/components/bucketSettings.vue
+++ src/view/main/oss/components/bucketSettings.vue
@@ -8,7 +8,7 @@
     >
     <el-form ref="form" :model="settings" label-width="80px" label-position="left">
       <el-form-item :label="$t('oss.settings.public')">
-        <el-switch v-model="settings.public"></el-switch>
+        <el-switch v-model="settings.public" @change="publicChanged"></el-switch>
       </el-form-item>
     </el-form>
   </el-drawer>
@@ -16,6 +16,9 @@
 
 <script>
 export default {
+  props: {
+    bucket: String,
+  },
   data() {
     return {
       show: false,
@@ -28,6 +31,45 @@ export default {
     open(policy) {
       this.settings.public = policy?.public || false;
       this.show = true;
+    },
+    publicChanged(val) {
+      if (val) {
+        this.settings.public = false;
+        this.$confirm(`${this.$t('oss.settings.enablePublic')}`, `${this.$t('confirm')}`)
+          .then(async () => {
+            const ret = await this.$nApi.post('/oss/setBucketPolicy', {
+              bucketName: this.bucket,
+              policy: {
+                public: true,
+              },
+            });
+            if (!ret) {
+              return;
+            }
+            this.$message.success(this.$t('oss.settings.success'));
+            this.settings.public = true;
+            this.$emit('policy-updated', { bucket: this.bucket });
+          })
+          .catch(() => {});
+      } else {
+        this.settings.public = true;
+        this.$confirm(`${this.$t('oss.settings.disablePublic')}`, `${this.$t('confirm')}`)
+          .then(async () => {
+            const ret = await this.$nApi.post('/oss/setBucketPolicy', {
+              bucketName: this.bucket,
+              policy: {
+                public: false,
+              },
+            });
+            if (!ret) {
+              return;
+            }
+            this.settings.public = false;
+            this.$message.success(this.$t('oss.settings.success'));
+            this.$emit('policy-updated', this.bucket);
+          })
+          .catch(() => {});
+      }
     },
   },
 };
