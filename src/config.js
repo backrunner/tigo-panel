d15400774ca01102cbb958772f0ed9a3c2b5860f
BackRunner <dev@backrunner.top>
2021-05-20 18:11:17 +0800

    fix: track config.js

---
+++ src/config.js
@@ -0,0 +1,15 @@
+const getConfig = () => {
+  return process.env.NODE_ENV === 'dev'
+    ? {
+      https: false,
+      host: 'localhost:24232',
+      base: '/api',
+    }
+    : {
+      https: true,
+      host: 'api.tigo.pwp.app',
+      base: '',
+    };
+};
+
+export default getConfig();
